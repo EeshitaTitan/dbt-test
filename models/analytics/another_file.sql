3c7b67b3801e1769354274deba9c26caec7dd009
Eeshita Dayani <eeshita@titan.com>
2024-11-26 14:30:10 -0500

    Added zip file

---
+++ models/analytics/another_file.sql
@@ -0,0 +1,6 @@
+SELECT  *
+FROM    (
+        VALUES
+        ('eeshita', 'dayani'),
+        ('evan', 'smith')
+        ) AS q (col1, col2)
---
+++ models/analytics/users_transformed.sql
@@ -0,0 +1,6 @@
+SELECT  *
+FROM    (
+        VALUES
+        (1, 2),
+        (3, 4)
+        ) AS q (col1, col2)
